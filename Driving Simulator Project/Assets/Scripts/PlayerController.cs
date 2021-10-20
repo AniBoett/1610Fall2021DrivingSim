2a26313c661bf5c0b3a42730d58d407969f440f2
Anika Boettcher <10945351@my.uvu.edu>
2021-10-19 20:06:49 -0600

    Tidied up workspace

--- Driving Simulator Project/Assets/Scripts/PlayerController.cs
+++ Driving Simulator Project/Assets/Scripts/PlayerController.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 5.0f;
+  private float speed = 20.0f;
 
-    public float turnSpeed;
-    public float horizontalInput;
-    public float forwardInput; 
+   private float turnSpeed = 45.0f;
+private float horizontalInput;
+   private float forwardInput; 
     
     // Start is called before the first frame update
     void Start()
@@ -21,8 +21,8 @@ public class PlayerController : MonoBehaviour
     {
         horizontalInput = Input.GetAxis("Horizontal");
         forwardInput = Input.GetAxis("Vertical");
-        //Move the vehicle forward
+        //Moves the vehicle forward
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
-        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
     }
 }
